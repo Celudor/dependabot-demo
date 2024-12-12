e0335582ea31bd6c06a7c7160c6a2d39316e39d7
Lukasz Stasienko <lukasz.stasienko@capgemini.com>
2024-12-12 09:07:24 +0100

    add terrform

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.35.0, <5.0.0"
+    }
+  }
+  backend "local" {
+    path = "terraform.tfstate"
+
+  }
+  required_version = "1.6.1"
+}
